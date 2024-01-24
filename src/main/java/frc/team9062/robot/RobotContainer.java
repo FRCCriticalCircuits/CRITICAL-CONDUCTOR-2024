038d99372e86baf54b9ff6a339b580314189e3e5
Williams <williams.edi@gmail.com>
2024-01-24 10:38:18 -0500

    Added Chooser for verb and auto

--- src/main/java/frc/team9062/robot/RobotContainer.java
+++ src/main/java/frc/team9062/robot/RobotContainer.java
@@ -4,16 +4,24 @@
 
 package frc.team9062.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team9062.robot.Commands.TeleopDrive;
 import frc.team9062.robot.Subsystems.SwerveSubsystem;
-import frc.team9062.robot.Util.RobotState.VERBOSITY_LEVEL;
+import frc.team9062.robot.Util.SystemState.VERBOSITY_LEVEL;
 
 public class RobotContainer {
   private SwerveSubsystem swerve;
   public SendableChooser<VERBOSITY_LEVEL> verbosityChooser = new SendableChooser<>();
+  public SendableChooser<String> autoChooser = new SendableChooser<>();
 
   public RobotContainer() {
     swerve = SwerveSubsystem.getInstance();
@@ -24,6 +32,17 @@ public class RobotContainer {
     verbosityChooser.addOption("HIGH", VERBOSITY_LEVEL.HIGH);
     verbosityChooser.addOption("COMP", VERBOSITY_LEVEL.COMP);
 
+    autoChooser.addOption("6 Piece", "6 Piece");
+    autoChooser.addOption("5 Piece", "5 Piece");
+    autoChooser.addOption("4 Piece Inside", "4 Piece Inside");
+    autoChooser.addOption("4 Piece Inside - Fast", "4 Piece Inside - Fast");
+    autoChooser.addOption("Test Auto", "Test Auto");
+
+    //autoChooser = AutoBuilder.buildAutoChooser();
+
+    SmartDashboard.putData("VERBOSITY", verbosityChooser);
+    SmartDashboard.putData("AUTO", autoChooser);
+
     swerve.setDefaultCommand(
       new TeleopDrive()
     );
@@ -32,6 +51,11 @@ public class RobotContainer {
   private void configureBindings() {}
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    Pose2d pose = PathPlannerAuto.getStaringPoseFromAutoFile(autoChooser.getSelected());
+
+    return new SequentialCommandGroup(
+      new InstantCommand(() -> swerve.resetOdom(pose), swerve),
+      AutoBuilder.buildAuto(autoChooser.getSelected())
+    );
   }
 }
