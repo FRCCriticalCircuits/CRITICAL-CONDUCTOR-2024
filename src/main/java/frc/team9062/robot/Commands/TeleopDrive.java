5833a51f10c4ce74aae4ffa60dbcdc7cbb8ff252
Williams <williams.edi@gmail.com>
2024-02-02 10:06:34 -0500

    Todo: Complete Command

--- src/main/java/frc/team9062/robot/Commands/TeleopDrive.java
+++ src/main/java/frc/team9062/robot/Commands/TeleopDrive.java
@@ -3,19 +3,22 @@ package frc.team9062.robot.Commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.team9062.robot.Subsystems.SwerveSubsystem;
 import frc.team9062.robot.Util.CriticalDeadband;
+import frc.team9062.robot.Util.IO;
 import frc.team9062.robot.Util.SwerveDriveController;
 import frc.team9062.robot.Util.gamepadConfigs;
 
 public class TeleopDrive extends Command{
     private SwerveSubsystem swerve;
-    private gamepadConfigs controls;
     private SwerveDriveController controller;
-    private CriticalDeadband deadband = new CriticalDeadband(0.1);
+    private CriticalDeadband deadband = new CriticalDeadband(0.15);
+    private IO io;
+    private boolean headingMode = false;
 
     public TeleopDrive() {
         swerve = SwerveSubsystem.getInstance();
-        controls = gamepadConfigs.getInstance();
-        controller = new SwerveDriveController(false);
+        gamepadConfigs.getInstance();
+        controller = new SwerveDriveController(true);
+        io = IO.getInstance();
 
         addRequirements(swerve);
     }
@@ -26,12 +29,20 @@ public class TeleopDrive extends Command{
 
     @Override
     public void execute() {
-        controller.drive(
-            deadband.applydeadband(() -> controls.getDriverController().getLeftY()), 
-            deadband.applydeadband(() -> controls.getDriverController().getLeftX()), 
-            deadband.applydeadband(() -> controls.getDriverController().getRightX()), 
-            true
-        );
+        if (io.getDriverYButton()) {
+            swerve.zeroGyro();
+        };
+
+        if (!headingMode) {
+            controller.drive(
+                deadband.applydeadband(() -> io.getDriverLeftY()), 
+                deadband.applydeadband(() -> io.getDriverLeftX()), 
+                deadband.applydeadband(() -> io.getDriverRightX()), 
+                true
+            );
+        } else {
+            
+        }
     }
 
     @Override
