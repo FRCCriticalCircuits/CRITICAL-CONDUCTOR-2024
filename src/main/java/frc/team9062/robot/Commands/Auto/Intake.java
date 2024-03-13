8db351bedb48032807056d4ea72afc98308ad791
Williams <williams.edi@gmail.com>
2024-03-12 22:51:00 -0400

    Added intake command

---
+++ src/main/java/frc/team9062/robot/Commands/Auto/Intake.java
@@ -0,0 +1,33 @@
+package frc.team9062.robot.Commands.Auto;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.team9062.robot.Subsystems.Arm;
+import frc.team9062.robot.Subsystems.Rollers;
+import frc.team9062.robot.Subsystems.Arm.ARM_STATE;
+import frc.team9062.robot.Subsystems.Rollers.INTAKE_STATE;
+
+public class Intake extends Command {
+    Arm arm;
+    Rollers rollers;
+
+    public Intake() {
+        arm = Arm.getInstance();
+        rollers = Rollers.getInstance();
+
+        addRequirements(arm, rollers);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        arm.setArmState(ARM_STATE.INTAKE);
+        rollers.setIntakeState(INTAKE_STATE.INTAKING);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return rollers.isWithGamePiece();
+    }
+}
